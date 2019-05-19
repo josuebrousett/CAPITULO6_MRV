02c51832d5892ad6c622895bbcbb6f142d49a0c2
josuebrousett <44821995+josuebrousett@users.noreply.github.com>
2019-05-19 12:54:52 -0500

    Add files via upload

---
+++ scripts/GestureWalk.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureWalk : MonoBehaviour {
+	private HeadLookWalk lookWalk;  
+	private HeadGesture gesture;
+
+	void Start () {
+		lookWalk = GetComponent<HeadLookWalk> ();    
+		gesture = GameObject.Find ("GameController"). GetComponent<HeadGesture> ();
+	}
+
+	void Update () {
+		if (gesture.isMovingDown) {      
+			lookWalk.isWalking = !lookWalk.isWalking;    
+		} 
+	}
+}
---
+++ scripts/HeadLookWalk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadLookWalk : MonoBehaviour {
+
+	public float velocity = 0.7f;
+	public bool isWalking = false;
+
+	private CharacterController controller;
+
+	private Clicker clicker = new Clicker();
+
+	void Start(){
+		controller = GetComponent<CharacterController> ();
+	}
+
+	void Update () {    
+		if(clicker.clicked()){
+			isWalking = !isWalking;
+		} 
+		if(isWalking){
+			controller.SimpleMove(Camera.main.transform.forward * velocity); 
+		}
+	}
+}
